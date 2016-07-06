dfb36d27c68e3053a4ace2a9517ddd15ca873d5f
fernando carvajal <efcu93@gmail.com>
2016-07-06 11:23:14 -0500

    cacasda

--- js/app.js
+++ js/app.js
@@ -1,29 +1,37 @@
-function mainde(){
-	var url = "../json/users.json"
-	$.getJSON("test.json", function(respuesta){
-		respuesta.users.forEach(function(user){
-			if (user.name == "radix"){
-				function Redirect() {
-	               window.location="../index/usuario.html";
-	            }
-			}
-			if (user.name == "pancake"){
-				function Redirect() {
-	               window.location="../index/operario.html";
-	            }
-			}
-			if (user.name == "quick"){
-				function Redirect() {
-	               window.location="../index/laboratorista.html";
-	            }
-			}
-		});
-	});
+function main(){
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if (xhttp.readyState == 4 && xhttp.status == 200){
+			var json = JSON.parse(xhttp.responseText);			
+			json.forEach(function(user){
+				var txtInput = document.querySelector(".login input");
+
+				if (user.username == txtInput.value){
+					if (user.id == 1){
+						console.log(txtInput.value);
+			            location.href = "index/usuario.html";
+		        	}
+		        	else if (user.id == 2){
+		        		console.log(txtInput.value);
+			            location.href = "index/operario.html";
+		        	}
+		        	else if (user.id == 3){
+		        		console.log(txtInput.value);
+			            location.href = "index/laboratorista.html";
+		        	}
+				}
+			});
+			
+		}
+	};
+	xhttp.open("GET", "json/users4.json",true);
+	xhttp.send();
 }
 
 function resgistrarEventos() {
     var item = document.getElementById("one");
-    item.addEventListener("click", mainde, false);
+    item.addEventListener("click", main, false);
 }
 
 window.addEventListener("load", resgistrarEventos, false);
