30e95951fb04056e0811c44429cb6ffbe1380709
Fernando Carvajal <efcu93@gmail.com>
2018-01-27 20:45:42 -0500

    agregando pestañas

--- apps/ia/scripts/app.js
+++ apps/ia/scripts/app.js
@@ -1,52 +1,28 @@
 ﻿angular.module('app', ['ui.router'])
     .config(["$stateProvider", "$compileProvider", function ($stateProvider, $compileProvider) {
         $stateProvider
-            .state('home', {
-                templateUrl: '/apps/ia/views/home.html',
-                controller: 'home'
-            })
             .state('documento', {
                 templateUrl: '/apps/ia/views/documento.html',
                 controller: 'documento'
             })
-            .state('grafico', {
-                templateUrl: 'views/grafico.html',
-                controller: 'grafico'
+            .state('diapositiva', {
+                templateUrl: '/apps/ia/views/diapositiva.html',
+                controller: 'diapositiva'
             })
-            .state('grafico.matrix', {
-                templateUrl: 'views/grafico.matrix.html',
-                controller: 'grafico.matrix'
-            })
-            .state('grafico.barras', {
-                templateUrl: 'views/grafico.barras.html',
-                controller: 'grafico.barras'
-            })
-            .state('listener', {
-                templateUrl: 'views/stream.html',
-                controller: 'listener'
-            })        
+            .state('aplicativo', {
+                templateUrl: '/apps/ia/views/aplicativo.html',
+                controller: 'aplicativo'
+            })     
     }])
     .run(["$state", "$http", "$templateCache", function ($state, $http, $templateCache) {
         $state.go('documento')
     }])
-    .factory("data", [function(){
+    .controller('documento', ["$scope", "$state", "$http", function($scope, $state, $http){
 
     }])
-    .controller('documento', ["$scope", "$state", "$http", "data", function($scope, $state, $http, data){
+    .controller('diapositiva', ["$scope", "$state", function($scope, $state){
 
     }])
-    .controller('listener', ["$scope", "$state", function($scope, $state){
-
-    }])
-    .controller('grafico', ["$scope", "$state", "data", '$rootScope', function ($scope, $state, data, $rootScope) {
-
-    }])
-    .controller('grafico.matrix', ["$scope", "$state", "data", function ($scope, $state, data){
-
-    }])
-    .controller('grafico.barras', ["$scope", "$state", "data", function ($scope, $state, data){
-
-    }])
-    .controller("home", [function () {
+    .controller('aplicativo', ["$scope", "$state", '$rootScope', function ($scope, $state, $rootScope) {
 
     }])
