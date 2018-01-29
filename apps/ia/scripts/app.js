88f8c16595e436655ae5bc6bc58af2114278a31b
Fernando Carvajal <efcu93@gmail.com>
2018-01-28 19:29:32 -0500

    Fin aplicativo, y pagina presentacion

--- apps/ia/scripts/app.js
+++ apps/ia/scripts/app.js
@@ -16,17 +16,37 @@
     }])
     .run(["$state", "$http", "$templateCache", function ($state, $http, $templateCache) {
         loadTemplates($state, "aplicativo", $http, $templateCache)
-
     }])
     .controller('aplicativo', ["$scope", function ($scope) {
 
+        var operaciones = { 
+            AND: [0,0,0,1],
+            XNOR: [1,0,0,1],
+            XOR: [0,1,1,0]
+        }
+
         $scope.factor = 0.9
         $scope.iteraciones = 600000
+        $scope.resultado = null //Resultado de la generación de operaciones
+        $scope.estimador = false //Muestra u oculta el panel de calculo de estimador
+        $scope.operacion_nombre = null //Nombre de la operacion que se esta calculando
 
-        $scope.calcular = async function (factor, iteraciones) {
+        $scope.cambiar_op = function(operacion) {
+            if (operacion !== $scope.operacion_nombre)
+                $scope.estimador = false;
+            else
+                $scope.estimador = true;
+        }
+
+        $scope.calcular = async function (factor, iteraciones, operacion) {
+
+            if (!operacion)
+                return alert("Ingrese una operacion")
+
+            $scope.operacion_nombre = operacion
             waitingDialog.show('NN en curso, por favor espere');
             console.time('Execution time')
-            var output = [0,0,0,1]
+            var output = operaciones[operacion]
             var worker = new GenericWebWorker(output, factor, iteraciones, Matrix, sigmoid, redNeuronal)
 
             var res = await worker.exec((output, factor, iter, Matrix, sigmoid, redNeuronal) => {
@@ -36,7 +56,8 @@
             })
             waitingDialog.hide()
             $scope.resultado = res
-            $scope.$apply()
+            $scope.estimador = true
+            $scope.$apply()            
             //console.log(res)
             console.timeEnd('Execution time')
             console.log('\n')
@@ -44,6 +65,10 @@
             console.log('XNOR 0 0', XNOR(0, 0, res))
             console.log('XNOR 1 1', XNOR(1, 1, res))
             console.log('XNOR 0 1', XNOR(0, 1, res))
+        }
+
+        $scope.estimar = function (val1, val2) {
+            $scope.resultado_estimar = XNOR(val1, val2, $scope.resultado)
         }
 
     }])
