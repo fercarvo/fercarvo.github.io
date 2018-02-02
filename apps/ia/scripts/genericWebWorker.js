8f9f05b2ba3da033c717d16e6d408e3ba76b5051
Fernando Carvajal <efcu93@gmail.com>
2018-02-02 00:37:31 -0500

    GenericWebWorker

--- apps/ia/scripts/genericWebWorker.js
+++ apps/ia/scripts/genericWebWorker.js
@@ -1,5 +1,5 @@
 /**
-* GenericWebWorker v2.1
+* GenericWebWorker v2.3
 * 
 * A Generic Javascript WebWorker that allows the user to execute and pass functions
 * Is Fileless, for what the user does not need a file.js to request
@@ -11,8 +11,8 @@ class GenericWebWorker {
     /**
     * @param {any} data, Any kind of arguments that will be used in the callback, functions too
     */
-    constructor(...ags) {
-        this.args = ags.map(a => (typeof a == 'function') ? {type:'fn', fn:a.toString()} : a)
+    constructor(...data) {
+        this.args = data.map(a => (typeof a === 'function') ? {type:'fn', fn:a.toString()} : a)
     }
 
     //@param {function} cb, To be executed, the params must be the same number of passed in the constructor 
@@ -23,21 +23,21 @@ class GenericWebWorker {
         var wk_string = this.worker.toString();
         wk_string = wk_string.substring(wk_string.indexOf('{') + 1, wk_string.lastIndexOf('}'));            
         var wk_link = window.URL.createObjectURL( new Blob([ wk_string ]) );
-        var wk = new Worker(wk_link);
+        var wk = new Worker( wk_link );
 
-        wk.postMessage({ callback: cb.toString(), args: this.args });
- 
-        var res = await new Promise((next, error) => {
-            wk.onmessage = e => (e.data && e.data.error) ? error(e.data.error) : next(e.data);
-            wk.onerror = e => error(e.message);        
-        }).finally(()=> { wk.terminate(), window.URL.revokeObjectURL(wk_link) })
-
-        return res
+        try { 
+            wk.postMessage({ callback: cb.toString(), args: this.args });
+            var res = await new Promise((next, error) => {
+                wk.onmessage = e => (e.data && e.data.error) ? error(e.data.error) : next(e.data);
+                wk.onerror = e => error(e.message);        
+            });            
+            return res
+        } catch (e) { throw e } finally { wk.terminate(), window.URL.revokeObjectURL(wk_link) }
     }
 
     //@param {function} cb, To be executed, the 1st argument is each element of the array
     async parallel(arr, cb) {
-        var res = [...arr].map(it => new GenericWebWorker(it, ...this.args).exec(cb))
+        var res = [...arr].map(it => new GenericWebWorker(it, ...this.args).exec(cb));
         return await Promise.all(res)
     }
 
@@ -45,13 +45,13 @@ class GenericWebWorker {
         onmessage = async function (e) {
             try {                
                 var cb = new Function(`return ${e.data.callback}`)();
-                var args = e.data.args.map(p => (p.type == 'fn') ? new Function(`return ${p.fn}`)() : p);
+                var args = e.data.args.map(p => (p.type === 'fn') ? new Function(`return ${p.fn}`)() : p);
 
                 try {
-                    var result = await cb(...args);//.apply(this, args); //If it is a promise or async function
+                    var result = await cb(...args)//If it is a promise or async function
                     return postMessage(result)
 
-                } catch (e) { throw new Error(`CallbackError: ${e}`) }
+                } catch (e) { throw `CallbackError: ${e}` }
             } catch (e) { postMessage({error: e.message}) }
         }
     }
